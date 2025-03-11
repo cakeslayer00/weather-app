cafbab966e9b83b7ead456b8876a548afcc841f8
vlad <mainacc1838@gmail.com>
2025-03-11 11:42:16 +0500

    add location controller

--- src/main/java/com/vladsv/weather_app/controller/LocationController.java
+++ src/main/java/com/vladsv/weather_app/controller/LocationController.java
@@ -1,22 +1,83 @@
 package com.vladsv.weather_app.controller;
 
-import com.vladsv.weather_app.entity.Location;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.JdkClientHttpConnector;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @RestController
-@RequestMapping(value = "/location")
+@RequestMapping(value = "/locations")
 @RequiredArgsConstructor
+@PropertySource("classpath:application.properties")
 public class LocationController {
 
-    @GetMapping
-    public Location getWeatherByLocationName(@RequestParam(value = "city") String city) {
-        System.out.println(city);
+    private static final String WEATHER_DATA_URL = "https://api.openweathermap.org/data/2.5";
+    private static final String WEATHER_GEO_URL = "https://api.openweathermap.org/geo/1.0";
 
-        return null;
+    @Value("${bean.appid}")
+    private String api;
+
+    @GetMapping("/search")
+    public String getSeveralLocationsByName(@RequestParam(value = "q") String city) {
+        WebClient webClient = getConfiguredWebClientBasedOnWeatherGeo();
+
+        return webClient.get()
+                .uri(
+                        uriBuilder -> uriBuilder.path("/direct")
+                                .queryParam("q", city)
+                                .queryParam("appid", api).build()
+                )
+                .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class)
+                .onErrorResume(e -> Mono.empty())
+                .block();
+    }
+
+    @GetMapping("/{city}")
+    public String getWeatherByCityName(@PathVariable String city) {
+        WebClient webClient = getConfiguredWebClientBasedOnWeatherData();
+
+        return webClient.get()
+                .uri(
+                        uriBuilder -> uriBuilder.path("/weather")
+                                .queryParam("q", city)
+                                .queryParam("appid", api).build()
+                )
+                .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class)
+                .onErrorResume(e -> Mono.empty())
+                .block();
+    }
+
+    private WebClient getConfiguredWebClientBasedOnWeatherGeo() {
+        return getConfiguredWebClient(WEATHER_GEO_URL);
+    }
+
+    private WebClient getConfiguredWebClientBasedOnWeatherData() {
+        return getConfiguredWebClient(WEATHER_DATA_URL);
+    }
+
+    private static WebClient getConfiguredWebClient(String url) {
+        ClientHttpConnector connector = getClientHttpConnector();
+
+        return WebClient.builder()
+                .baseUrl(url)
+                .clientConnector(connector).build();
+    }
+
+    private static ClientHttpConnector getClientHttpConnector() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+
+        return new JdkClientHttpConnector(httpClient);
     }
 
 }
